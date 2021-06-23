56db87d90fd33a2609267de32b08990c5859071c
Margin.xu <margin.xu@ringcentral.com>
2021-06-23 15:13:55 +0800

    [list] use rule to optimize code

--- LearningCode/src/main/java/com/java/Inversion/DependencyInversion.java
+++ LearningCode/src/main/java/com/java/Inversion/DependencyInversion.java
@@ -4,17 +4,25 @@ public class DependencyInversion {
     public static void main(String[] args) {
         Person person = new Person();
         person.receive(new Email());
+        person.receive(new Wechat());
     }
 }
 
-class Email{
+interface Receiver{
+    public String getInfo();
+}
+class Email implements Receiver{
     public String getInfo(){
         return "这是一条电子邮件信息";
     }
 }
-
+class Wechat implements Receiver{
+    public String getInfo() {
+        return "这是一条微信消息";
+    }
+}
 class Person{
-    public void receive(Email email){
-        System.out.println(email.getInfo());
+    public void receive(Receiver receiver){
+        System.out.println(receiver.getInfo());
     }
 }
